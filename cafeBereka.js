e4141fa73e0d1b44607d30bdbb83c46dd53ef738
Gerardo Perez Rocha <gerardoperezrocha@gmail.com>
2023-02-28 15:24:46 -0600

    MORE JS

--- cafeBereka.js
+++ cafeBereka.js
@@ -17,5 +17,11 @@ function  Producto(name, origin, finca, altitude, variety, price, availability, 
 
 
 const producto1 = new Producto('Chiapas Lavado', 'Chiapas', 'colectivo', '1200', 'caturra', '600', 2, 'pic', 'cafe lavado de chiapas');
+const producto2 = new Producto('Chiapas Marago','Chiapas', 'colectivo', '1200', 'Marago', 350, 3, 'pic', 'El café marago bla bla bla');
+
+const createProduct = (producto) => {
+  console.log(producto);
+
+}
 
 console.log(producto1.altitude);
