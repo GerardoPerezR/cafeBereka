897e3e1b4ead4ddfe29269e5372eb47cead3da77
Gerardo Perez Rocha <gerardoperezrocha@gmail.com>
2023-03-02 19:09:19 -0600

    CONTENT

--- cafeBereka.js
+++ cafeBereka.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
 //Create Product Prototype Object
 
 function  Producto(name, origin, finca, productor, altitude, variety, price, availability, pic, description, process)  {
+  const productCardsContainer = document.getElementById('products');
 
   this.name = name;
   this.origin = origin;
@@ -29,7 +30,8 @@ const producto1 = new Producto('Chiapas Lavado', 'Chiapas', 'colectivo', 'colect
 const producto2 = new Producto('Chiapas Marago','Chiapas', 'colectivo','colectivo',  '1200', 'Marago', 350, 3, 'DSC_5240_2.jpg', 'El café Maragopipe, o Marago, como se le conoce comúnmente en México, obtiene su nombre de la ciudad del mismo nombre, en Brazildonde apareció por primera vez esta mutación de la variedad Typica, en el año de 1870. A sus granos más grandes de lo normal también se les conoce como granos elefante. Es un excelente grano, apreciado por consumidores y conocedores en todo el mundo, especialmente cuando se cultiva a gran altura.');
 const producto3 = new Producto('La Herradura', 'Veracruz', 'La Herradura', 'Roberto Licona', 1370, 'Guernica', 700, 2, 'DSC_5232~3.jpg', 'La finca La Herradura se ubica en la reconocida región cafetalera de Coatepec del Estado de Veracruz, identificada por sus condiciones privilegiadas de clima y geografía para un óptimo desarrollo de cafetos de la especie arábica.', 'lavado' );
 const producto4 = new Producto('Manos de Mujer', 'Oaxaca', 'Finca Chelín', 'Enrique López', 1550, 'Gesha', 600, 3, 'pic', 'El proyecto café manos de mujeres Oaxaqueña, es un reconocimiento a las Mujeres por su gran labor social y ambientalmente responsable, además contribuye a mitigar los efectos de sobrecalentamiento ocasionados por el cambio climático. Café de especialidad por un Oaxaca sustentable y próspero. Descripción:  Lavado de mujeres de Candelaria Se reposan 12 horas en cereza, se despulpan a la mañana siguiente y se fermentan en bolsas de plástico por 24 horas, se lavan y se inicia su secado por unas 15 horas. Est grano es de variedad Gesha, también conocido como Geisha.')
-const producto5 = new Producto('Descafeinado', 'Chiapas', 'Colectivo', 'Colectivo', 1300, 'mezcla', 2, 'pic', 'pic', 'lavado descafeinado' )
+const producto5 = new Producto('Descafeinado', 'Chiapas', 'Colectivo', 'Colectivo', 1300, 'mezcla', 2, 'pic', 'pic', 'lavado descafeinado' );
+
 //function which places card with product information. Use console.log for now
 
 
