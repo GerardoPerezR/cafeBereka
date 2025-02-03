9a6748b09a2a668e9821533be2c3ded4fd969d4a
Gerardo Perez Rocha <gerardoperezrocha@Gerardos-MacBook-Air.local>
2025-02-02 18:05:06 -0600

    kjshdfksjdhf

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 // Define database connection variables
 $servername = "localhost"; // This is usually 'localhost', but confirm it with Hostinger if you're unsure
-$username = "u239129368_root"; // Your MySQL username (created in phpMyAdmin)
-$password = "antena"; // Your MySQL password
-$dbname = "u239129368_cafeBereka";   // Your MySQL database name
+$username = "..."; // Your MySQL username (created in phpMyAdmin)
+$password = "..."; // Your MySQL password
+$dbname = "...";   // Your MySQL database name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
