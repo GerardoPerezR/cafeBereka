b99405c4f71613791f197b3b16e2035a3947f87d
Gerardo PR <69208233+GerardoPerezR@users.noreply.github.com>
2025-06-18 17:36:24 -0600

    style

--- public_html/redirect.php
+++ public_html/redirect.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../config.php'; // Adjust path if config.php is in a different location
+require_once __DIR__ . '/../config.php'; 
 
 $id = isset($_GET['id']) ? (int) $_GET['id'] : 0;
 
